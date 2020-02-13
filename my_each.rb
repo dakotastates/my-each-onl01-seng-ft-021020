922c081d52e883e65b1a083168ec6740e379f477
Dakota States <dakota.states@gmail.com>
2020-02-13 00:28:09 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,18 +1,11 @@
 def my_each(array)
   i = 0
-  while i < array.length
-
-    yield(array[i])
-    i += 1
-  end
-end
-
-def hello(array)
-  i = 0
   collection = []
   while i < array.length
+
     collection << yield(array[i])
     i += 1
   end
   collection
 end
+
